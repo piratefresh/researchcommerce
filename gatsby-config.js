71ab21a6db6d4f433e21b2e7945b5f42242962e0
piratefresh <magnussithnilsen@gmail.com>
2021-04-24 02:36:46 -0400

    Withlink show grid

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,11 +64,11 @@ module.exports = {
     {
       resolve: `gatsby-source-shopify`,
       options: {
-        shopName: process.env.GATSBY_SHOP_NAME, // Load from env
+        shopName: "setvi", // Load from env
         accessToken: process.env.GATSBY_SHOPIFY_ACCESS_TOKEN, // Load from env
         verbose: true,
         paginationSize: 100,
-        // includeCollections: ["shop"],
+        includeCollections: ["shop"],
       },
     },
     {
